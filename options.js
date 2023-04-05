19d1c8d09666284d9392873a01de22da83f65468
kovba_san <kovbas2311@gmail.com>
2023-04-05 17:49:26 +0300

    local storage fix

--- options.js
+++ options.js
@@ -1,3 +1,7 @@
+if (localStorage.getItem('world') == null) {
+  getReponse()
+}
+
 const options = {}
 
 localStorage.setItem('quiz-options', '')
@@ -110,4 +114,40 @@ function prepareQuizDifficultySelect() {
     window.location.href = './quiz.html'
     // console.log(options)
   })
+}
+
+async function getReponse() {
+  let response = await fetch('https://restcountries.com/v3.1/all')
+  let countries = await response.json()
+  const unMembers = []
+  const europe = []
+  const asia = []
+  const africa = []
+  const america = []
+  const oceania = []
+  for (let country of countries) {
+    if (country.unMember) {
+      unMembers.push(country)
+      switch (country.region) {
+        case 'Europe': europe.push(country)
+          break
+        case 'Asia': asia.push(country)
+          break
+        case 'Asia': asia.push(country)
+          break
+        case 'Americas': america.push(country)
+          break
+        case 'Africa': africa.push(country)
+          break
+        case 'Oceania': oceania.push(country)
+          break
+      }
+    }
+  }
+  localStorage.setItem('world', JSON.stringify(unMembers))
+  localStorage.setItem('europe', JSON.stringify(europe))
+  localStorage.setItem('asia', JSON.stringify(asia))
+  localStorage.setItem('africa', JSON.stringify(africa))
+  localStorage.setItem('america', JSON.stringify(america))
+  localStorage.setItem('oceania', JSON.stringify(oceania))
 }
--- quiz.js
+++ quiz.js
@@ -2,8 +2,6 @@ import startConfetti from "./confetti.js"
 
 const options = JSON.parse(localStorage.getItem('quiz-options'))
 
-checkLocalStorage()
-
 const questionArray = createQuestionsArray(options)
 
 renderPage(options)
@@ -19,50 +17,6 @@ showQuestion(currentQuestionIndex, options)
 let correctAnswers = 0
 
 // <<<<<<<<<<<<<<FUNCTIONS>>>>>>>>>>>>>>>
-
-function checkLocalStorage() {
-  if (!localStorage.getItem('world')) {
-    getReponse()
-  } else {
-    return
-  }
-}
-
-async function getReponse() {
-  let response = await fetch('https://restcountries.com/v3.1/all')
-  let countries = await response.json()
-  const unMembers = []
-  const europe = []
-  const asia = []
-  const africa = []
-  const america = []
-  const oceania = []
-  for (let country of countries) {
-    if (country.unMember) {
-      unMembers.push(country)
-      switch (country.region) {
-        case 'Europe': europe.push(country)
-          break
-        case 'Asia': asia.push(country)
-          break
-        case 'Asia': asia.push(country)
-          break
-        case 'Americas': america.push(country)
-          break
-        case 'Africa': africa.push(country)
-          break
-        case 'Oceania': oceania.push(country)
-          break
-      }
-    }
-  }
-  localStorage.setItem('world', JSON.stringify(unMembers))
-  localStorage.setItem('europe', JSON.stringify(europe))
-  localStorage.setItem('asia', JSON.stringify(asia))
-  localStorage.setItem('africa', JSON.stringify(africa))
-  localStorage.setItem('america', JSON.stringify(america))
-  localStorage.setItem('oceania', JSON.stringify(oceania))
-}
 
 function Question(item, answer) {
   this.questionItem = item
